e0f32ed3b300597839c50ee36fbdc860209891d2
Gabriel <gabriel.silveira20@bol.com.br>
2022-02-18 18:13:13 -0300

    Soma

---
+++ Calculadora.java
@@ -0,0 +1,7 @@
+public class Calculadora{
+
+    public double soma(double a, double b){
+        double r = a + b;
+        return r;
+    }
+}
---
+++ TesteCalculadora.java
@@ -0,0 +1,7 @@
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        System.out.println(soma(a,b));
+    }
+}
